5d447dcb71e90a1b73e9b26f5d754ab045c488db
Pascal Welsch <pascal.welsch@gmail.com>
2015-11-24 23:41:07 +0100

    working for loop
    I should check other implementations. this solution has too much code

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -15,7 +15,24 @@ enum class TimeInterval {
 }
 
 
-class DateRange(override val start: MyDate, override val endInclusive: MyDate) : Comparable<DateRange>, ClosedRange<MyDate> {
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : Comparable<DateRange>, ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = object : Iterator<MyDate> {
+        var now: MyDate = start
+
+        override fun next(): MyDate {
+            if (now === start) {
+                now = start.copy()
+                return now
+            }
+            now = now.nextDay()
+            return now
+        }
+
+        override fun hasNext(): Boolean {
+            return now.nextDay() <= endInclusive
+        }
+
+    }
 
     override fun compareTo(other: DateRange): Int = lenght() - other.lenght()
 
