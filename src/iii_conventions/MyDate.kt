9f0cb03b42f5fff289503ff074ffa0def9ac9c0b
Pascal Welsch <pascal.welsch@gmail.com>
2015-11-24 23:16:03 +0100

    in range and compare to was hard

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,10 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int = size() - other.size()
+
+    fun size(): Int = dayOfMonth + month * 100 + year * 10000
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
@@ -10,4 +14,11 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : Comparable<DateRange>, ClosedRange<MyDate> {
+    
+    override fun compareTo(other: DateRange): Int = lenght() - other.lenght()
+
+    fun lenght(): Int = endInclusive.size() - start.size()
+
+}
